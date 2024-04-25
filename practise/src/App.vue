43191641a270bcfe1c690401d23b8881b89bc759
Ashish Gautam <ashish.gautam.talentelgia@gmail.com>
2024-04-25 16:02:17 +0530

    minor update

---
+++ practise/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+</script>
+
+<template>
+    <p> Hello {{ count }}</p>
+</template>
+
+<style scoped>
+</style>
